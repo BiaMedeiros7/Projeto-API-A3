fc7611d4c1e4dfd18303b45341eb61984c2dfa5e
Beatriz Medeiros <98035518+BiaMedeiros7@users.noreply.github.com>
2024-11-27 10:22:14 -0300

    Update pytestSchemas.py

--- pytestSchemas.py
+++ pytestSchemas.py
@@ -3,10 +3,11 @@ from pydantic import ValidationError
 import pytest
 from schemas import TarefaOut
 
+#testes unificados
 
 def test_tarefa_create_nome_tarefa_obrigatorio():
     with pytest.raises(ValidationError):
-        TarefaCreate(nome_tarefa=None)  # Espera um erro, pois nome_tarefa não pode ser None
+        TarefaCreate(nome_tarefa=None)  
 
 
 
@@ -17,7 +18,7 @@ def test_tarefa_create_valida():
 
 def test_tarefa_create_invalida():
     try:
-        TarefaCreate(nome_tarefa=None)  # Nome da tarefa não pode ser None
+        TarefaCreate(nome_tarefa=None)  
     except ValidationError as e:
         assert "Input should be a valid string" in str(e)
 
@@ -28,24 +29,24 @@ def test_tarefa_update():
 
 
 def test_status_enum_valido():
-    # Testa se um valor válido é aceito
+    
     tarefa = TarefaCreate(nome_tarefa="Testar Enum", status_tarefa=StatusEnum.PENDENTE)
     assert tarefa.status_tarefa == StatusEnum.PENDENTE
 
 def test_status_enum_invalido():
-    # Testa se um valor inválido gera erro
+   
     with pytest.raises(ValueError):
         TarefaCreate(nome_tarefa="Testar Enum", status_tarefa="invalid_status")
 
 
 def test_tarefa_update_status_opcional():
     tarefa = TarefaUpdate(nome_tarefa="Atualizar Tarefa")
-    assert tarefa.status_tarefa is None  # Espera que status_tarefa seja None, pois não foi informado
+    assert tarefa.status_tarefa is None  
 
 
 def test_tarefa_update_nome_opcional():
     tarefa = TarefaUpdate(status_tarefa=StatusEnum.CONCLUIDO)
-    assert tarefa.nome_tarefa is None  # Espera que nome_tarefa seja None, pois não foi informado
+    assert tarefa.nome_tarefa is None 
 
 
 def test_tarefa_out():
@@ -61,15 +62,15 @@ def test_tarefa_create_valida():
 
 def test_tarefa_create_nome_tarefa_tipo_incorreto():
     with pytest.raises(ValidationError):
-        TarefaCreate(nome_tarefa=12345)  # Deve lançar erro, pois nome_tarefa deve ser uma string
+        TarefaCreate(nome_tarefa=12345) 
 
 def test_tarefa_create_com_valor_default():
     tarefa = TarefaCreate(nome_tarefa="Tarefa sem status")
-    assert tarefa.status_tarefa == StatusEnum.PENDENTE  # Valor padrão deve ser 'pendente'
+    assert tarefa.status_tarefa == StatusEnum.PENDENTE  
 
 def test_tarefa_to_json():
     tarefa = TarefaOut(idtarefas_api=2, nome_tarefa="Tarefa para exportar", status_tarefa=StatusEnum.CONCLUIDO)
-    tarefa_json = tarefa.dict()  # Converte para dicionário
+    tarefa_json = tarefa.dict() 
     assert "idtarefas_api" in tarefa_json
     assert "nome_tarefa" in tarefa_json
     assert "status_tarefa" in tarefa_json

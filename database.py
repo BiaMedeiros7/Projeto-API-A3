35b57294395a8811fb6ba5bd7a57a66c6d901c6a
Beatriz Medeiros <98035518+BiaMedeiros7@users.noreply.github.com>
2024-11-27 14:10:37 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,13 +1,10 @@
-
 ## conexão ao banco de dados
-
-
-from sqlalchemy import create_engine, Column, Integer, String, SmallInteger
+from sqlalchemy import create_engine, Column, Integer, String, Enum, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from enum import Enum as PyEnum
 
-
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:NovaSenha@localhost/banco-de-dados-api"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:sql070105@localhost/banco-de-dados-api"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
@@ -15,13 +12,17 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
 Base = declarative_base()
 
+class StatusEnum(PyEnum):
+    PENDENTE = "pendente"
+    CONCLUIDO = "concluido"
+
 
 class Tarefa(Base):
     __tablename__ = 'tarefas_api'  
 
     idtarefas_api = Column(Integer, primary_key=True, autoincrement=True)
     nome_tarefa = Column(String(200), nullable=False)
-    status_tarefa = Column(SmallInteger, nullable=False)
+    status_tarefa = Column(Enum(StatusEnum), nullable=False, default=StatusEnum.PENDENTE)
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -31,9 +32,6 @@ def create_tables():
     Base.metadata.create_all(bind=engine)
 
 
-from sqlalchemy.orm import Session
-
-# Função para criar uma nova tarefa
 def create_task(nome_tarefa, status_tarefa):
     db = SessionLocal()
     tarefa = Tarefa(nome_tarefa=nome_tarefa, status_tarefa=status_tarefa)
@@ -45,28 +43,3 @@ def create_task(nome_tarefa, status_tarefa):
 
 
 
-
-
-
-
-
-
-
-
-# Teste de inserção de dados
-#if __name__ == "__main__":
- #   create_tables()
- #   print("Tabela criada com sucesso!")
-    
-    # Teste de inserção
-#    nova_tarefa = create_task("Estudar para a prova", 1)
-#    print(f"Tarefa criada: {nova_tarefa.nome_tarefa} com status {nova_tarefa.status_tarefa}")
-
-
-
-## teste para ver se a conexão está funcioando
-#try:
-#    with engine.connect() as connection:
-#        print("Conexão bem-sucedida ao banco de dados!")
-#except Exception as e:
-#    print(f"Erro na conexão: {e}")

c4370b8111e39c44c8696920fdf589eb58950d7f
Beatriz Medeiros <98035518+BiaMedeiros7@users.noreply.github.com>
2024-12-04 14:10:41 -0300

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,36 +1,29 @@
 # arq de validação de dados
-
 from pydantic import BaseModel
-from enum import Enum
 from typing import Optional
-
-
-class StatusEnum(str, Enum):
-    PENDENTE = "pendente"
-    CONCLUIDO = "concluido"
-
+from pydantic import BaseModel
 
 class TarefaCreate(BaseModel):
     nome_tarefa: str
-    status_tarefa: StatusEnum = StatusEnum.PENDENTE  
+    status_tarefa: int = 0  # Padrão para PENDENTE
 
     class Config:
-        from_attributes = True 
-
+        from_attributes = True
 
 class TarefaOut(BaseModel):
     idtarefas_api: int
     nome_tarefa: str
-    status_tarefa: StatusEnum
+    status_tarefa: int
 
     class Config:
-       from_attributes = True 
+        from_attributes = True
 
 
 class TarefaUpdate(BaseModel):
-    nome_tarefa: Optional[str] = None
-    status_tarefa: Optional[StatusEnum] = None
+    nome_tarefa: str
+    status_tarefa: int
+
 
     class Config:
-        from_attributes = True 
+        from_attributes = True
 

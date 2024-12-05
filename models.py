24e047b772ecc8d55d174fafec41f1673ab11af8
Beatriz Medeiros <98035518+BiaMedeiros7@users.noreply.github.com>
2024-12-05 18:44:15 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,12 +1,12 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, SmallInteger
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class Tarefa(Base):
     __tablename__ = "tarefas_api"
-    __table_args__ = {"extend_existing": True}  # Evita conflito se a tabela já existir
 
     idtarefas_api = Column(Integer, primary_key=True, index=True)
     nome_tarefa = Column(String(200), index=True)
-    status_tarefa = Column(Integer, nullable=False, default=0)  # 0 para PENDENTE, 1 para CONCLUÍDO
+    status_tarefa = Column(SmallInteger, nullable=False, default=0)  
+    box_id = Column(Integer, nullable=False)  
